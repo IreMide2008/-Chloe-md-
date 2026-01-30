1267d2498b503f15446d2f35ecdb66faa019487a
Ⲇⲉⲁⲧⲏ ⲥⲟⲇⲉ <iremideakintaiwo@gmail.com>
2026-01-30 05:33:48 +0100

    Add initial settings configuration for chloe bot

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'chloe bot',
+  author: '‎',
+  botName: "chloe bot",
+  botOwner: 'Ⲇⲉⲁⲧⲏ ⲥⲟⲇⲉ', // Your name
+  ownerNumber: '2348085274623', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.6",
+  updateZipUrl: "https://github.com/minatonamikaze359/Minato-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
